8688d017f82fd56c29c9e52116bc58ea63aa1e3d
criticalisland <54601595+criticalisland@users.noreply.github.com>
2019-12-13 02:10:42 -0500

    Add files via upload

---
+++ numtowords-mvc-dash/src/main/java/com/sonatype/spring/model/Numbers.java
@@ -0,0 +1,31 @@
+package com.sonatype.spring.model;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.Pattern;
+
+@Entity
+public class Numbers {
+
+	@Pattern(regexp = "[0-9]{1,10}|[0-9]{1,3},[0-9]{1,3}|[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}|[0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}|-[0-9]{1,10}|-[0-9]{1,3},[0-9]{1,3}|-[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}|-[0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0-9]{1,3}", message = "Please enter valid number format like 123,456 or -567890")
+	public String numberStr;
+
+	public static String numberWords;
+
+	public String getNumberStr() {
+		return numberStr;
+	}
+
+	public void setNumberStr(String numberStr) {
+
+		this.numberStr = numberStr;
+	}
+
+	public String getNumberWords() {
+		return numberWords;
+	}
+
+	public void setNumberWords(String numberWords) {
+		Numbers.numberWords = numberWords;
+	}
+	
+}
